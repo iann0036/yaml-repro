0ea5ec9211869b1fa6e965c984e341ed5b6313d5
Ian Mckay <contact@ianmckay.com.au>
2020-09-09 18:56:38 +1000

    Sample code upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const parseCST = require('yaml/parse-cst');
+
+var cst = parseCST(`
+TopLevel:
+    SecondLevel1:
+        ThirdLevel1: Value1
+    SecondLevel2:
+        ThirdLevel2: Value2
+`)
+
+var secondLevelItems = cst[0].contents[1].items[1].node.items.splice(2, 2);
+
+cst[0].contents[1].items[1].node.items = secondLevelItems.concat(cst[0].contents[1].items[1].node.items);
+
+console.log(String(cst));
+
+/*
+
+expected:
+TopLevel:
+    SecondLevel1:
+        ThirdLevel1: Value1
+    SecondLevel2:
+        ThirdLevel2: Value2
+
+
+actual:
+TopLevel:
+    SecondLevel1:
+        ThirdLevel1: Value1
+    SecondLevel2:
+        ThirdLevel2: Value2
+SecondLevel1:
+        ThirdLevel1: Value1
+*/
